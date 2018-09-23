eb83c1df39dfe7ed6739939319b30d90a2c04223
RockThunderheart <alexdaigre@gmail.com>
2018-09-23 14:48:15 -0500

    working on non recursive traversals

--- binarySearchTree.cc
+++ binarySearchTree.cc
@@ -1,4 +1,6 @@
 #include <string>
+#include <stack>
+#include <list>
 using namespace std; 
 
 struct Node {
@@ -6,22 +8,89 @@ struct Node {
     Node *left;
     Node *right;
 
-    Node(string val) {
-        this->value = val;
-    }
-
-    Node(string val, Node *left, Node *right) {
-        this->value = val;
-        this->left = left;
-        this->right = right;
+    Node(string value = "") {
+        this->value = value;
+        this->left = NULL;
+        this->right = NULL;
     }
 };
 
 class BinarySearchTree { 
     private: 
-    Node rootNode;
+    Node *rootNode;
   
     void addNode(string value) { 
+        bool complete = false; 
+        Node *currentNode = this->rootNode;
+
+        if (currentNode->value == "") {
+            currentNode->value = value;
+            complete = true;
+        }
+
+        while (complete == false){ 
+            if (currentNode->value.compare(value) > 0) {
+                if (currentNode->value.compare(value) > 0) {
+                    if (!currentNode->right) {
+                        currentNode->right = new Node(value);
+                        complete = true;
+                    } else {
+                        currentNode = currentNode->right;
+                    }
+                } else {
+                    if (!currentNode->left) {
+                        currentNode->left = new Node(value);
+                        complete = true;
+                    } else {
+                        currentNode = currentNode->left;
+                    }
+                }
+            }
+        }
+    }
+
+    list<string> getPreOrderTraversal() {
+    }
+
+    list<string> printInOrderTraversal() {
+        Node *currentNode = this->rootNode;
+        list<string> traversalList;
+        stack<Node *> nodeStack;
+
+        if (currentNode->value == ""){
+            return traversalList;
+        }
+
+        while (currentNode != NULL || nodeStack.empty() == false) { 
+            while (currentNode !=  NULL) {
+                nodeStack.push(currentNode); 
+                currentNode = currentNode->left; 
+            }
+            currentNode = nodeStack.top(); 
+            nodeStack.pop(); 
+            
+            traversalList.push_back(currentNode->value); 
         
-    } 
-}; 
+            currentNode = currentNode->right;
+        }
+        return traversalList;
+    }
+
+    list<string> printPostOrderTraversal() {
+        Node *currentNode = this->rootNode;
+        list<string> traversalList;
+        stack<Node *> nodeStack;
+
+        if (currentNode->value == ""){
+            return traversalList;
+        }
+
+        while (currentNode) {
+            if (currentNode->right){
+                nodeStack.push(currentNode->right);
+            }
+            nodeStack.push(currentNode);
+            currentNode = currentNode->left;
+        }
+    }
+};
